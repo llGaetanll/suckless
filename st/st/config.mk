112d51f03a9b5a85a6074aa79de285c933049665
llGaetanll <gaetan.almela@gmail.com>
2020-06-01 09:53:27 -0400

    added modified Luke Smith st build

--- st/st/config.mk
+++ st/st/config.mk
@@ -1,14 +1,18 @@
 # st version
-VERSION = 0.8.3
+VERSION = 0.8.2
 
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX ?= /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
+
+# include X11 in Ubuntu
+# X11INC = /usr/include/X11R6
+# X11LIB = /usr/lib/X11R6
 
 PKG_CONFIG = pkg-config
 
@@ -16,7 +20,7 @@ PKG_CONFIG = pkg-config
 INCS = -I$(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
        `$(PKG_CONFIG) --cflags freetype2`
-LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
+LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender \
        `$(PKG_CONFIG) --libs fontconfig` \
        `$(PKG_CONFIG) --libs freetype2`
 
